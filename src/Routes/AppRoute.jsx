1af1f329ed8d473be3f5af6931fd33f478d0f51e
Iago Gondim <iago-gondim@hotmail.com>
2023-01-28 10:23:52 -0300

    add ProtectedRoute

--- src/Routes/AppRoute.jsx
+++ src/Routes/AppRoute.jsx
@@ -1,15 +1,24 @@
-import {  Route, Routes } from 'react-router-dom';
-import Home from '../Components/Home/Home';
-import Login from '../Components/Login/Login';
-
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "../Components/Helper/ProtectedRoute";
+import Home from "../Components/Home/Home";
+import Login from "../Components/Login/Login";
+import User from "../Components/User/User";
 
 const AppRoute = () => {
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login/*" element={<Login />} />
-      </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="login/*" element={<Login />} />
+      <Route
+        path="conta/*"
+        element={
+          <ProtectedRoute>
+            <User />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
 };
 
-export default AppRoute
+export default AppRoute;
