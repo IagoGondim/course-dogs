ea3a762fd1a18d67b46b0ee156b1761892688ce3
Iago Gondim <iago-gondim@hotmail.com>
2023-01-30 15:59:28 -0300

    add PhotoComments

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { UserContext } from "../../useContext/useContext";
+import styles from "./PhotoComments.module.css";
+import PhotoCommentsForm from "./PhotoCommentsForm";
 
-const PhotoComments = () => {
-  return <div></div>;
+const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => props.comments);
+  const commentsSection = useRef(null);
+  const { login } = useContext(UserContext);
+
+  useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
+
+  return (
+    <>
+      <ul ref={commentsSection} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
