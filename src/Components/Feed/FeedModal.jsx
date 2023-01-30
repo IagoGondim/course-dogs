4dc8512ab5166cf9a8b49a3ca8159e0fca75c4fb
Iago Gondim <iago-gondim@hotmail.com>
2023-01-30 09:53:40 -0300

    add FeedModal

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { PHOTO_GET } from "../../Api/api";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import PhotoContent from "../Photo/PhotoContent";
+import styles from "./FeedModal.module.css";
 
-const FeedModal = () => {
-  return <div>FeedModal</div>;
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  const handleOutsideClick = (event) => {
+    if (event.target === event.currentTarget) setModalPhoto(null);
+  };
+
+  return (
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;
--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,19 +1,18 @@
 import React, { useEffect } from "react";
 import { PHOTOS_GET } from "../../Api/api";
 import useFetch from "../../Hooks/useFetch";
-import Loading from "../Header/Loading";
 import Error from "../Helper/Error";
 import FeedPhotosItem from "./FeedPhotosItem";
 import styles from "./FeedPhotos.module.css";
+import Loading from "../Helper/Loading";
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
 
   useEffect(() => {
     async function fetchPhotos() {
       const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
       const json = await request(url, options);
-      console.log(json);
     }
     fetchPhotos();
   }, [request]);
@@ -24,7 +23,11 @@ const FeedPhotos = () => {
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) => (
-          <FeedPhotosItem key={photo.id} photo={photo} />
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
         ))}
       </ul>
     );
