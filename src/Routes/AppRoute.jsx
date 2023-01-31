c3a9c86620f1be2076fe8491dfd0d638727a6e88
Iago Gondim <iago-gondim@hotmail.com>
2023-01-31 15:05:23 -0300

    add perfil user route

--- src/Routes/AppRoute.jsx
+++ src/Routes/AppRoute.jsx
@@ -4,6 +4,7 @@ import Home from "../Components/Home/Home";
 import Login from "../Components/Login/Login";
 import Photo from "../Components/Photo/Photo";
 import User from "../Components/User/User";
+import UserProfile from "../Components/User/UserProfile";
 
 const AppRoute = () => {
   return (
@@ -19,6 +20,7 @@ const AppRoute = () => {
         }
       />
       <Route path="foto/:id" element={<Photo />} />
+      <Route path="perfil/:user" element={<UserProfile />} />
     </Routes>
   );
 };
