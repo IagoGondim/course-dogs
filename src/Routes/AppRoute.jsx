19b28292a9ad51a3d8766be72a6338405e3839e6
Iago Gondim <iago-gondim@hotmail.com>
2023-01-31 15:14:06 -0300

    add route page not found 404

--- src/Routes/AppRoute.jsx
+++ src/Routes/AppRoute.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "../Components/Helper/ProtectedRoute";
 import Home from "../Components/Home/Home";
 import Login from "../Components/Login/Login";
+import NotFound from "../Components/NotFound";
 import Photo from "../Components/Photo/Photo";
 import User from "../Components/User/User";
 import UserProfile from "../Components/User/UserProfile";
@@ -21,6 +22,7 @@ const AppRoute = () => {
       />
       <Route path="foto/:id" element={<Photo />} />
       <Route path="perfil/:user" element={<UserProfile />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
