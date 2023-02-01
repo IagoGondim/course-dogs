5d2f0d8e5dbffcf8b29eb178671cce346fd7ff83
Iago Gondim <iago-gondim@hotmail.com>
2023-02-01 10:55:31 -0300

    settings finals

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { STATS_GET } from "../../Api/api";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-  return <div>UserStats</div>;
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
